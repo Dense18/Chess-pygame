68734f2c065ba4debb827ec0a5f881b427dbc8bf
George <george-andreas@hotmail.com>
2023-12-18 18:48:44 -0800

    Update page locators according to the new html tag

--- bot/PageLocators.py
+++ bot/PageLocators.py
@@ -1,11 +1,13 @@
 from selenium.webdriver.common.by import By
 
+
 class PageLocators :
     """
     Class storing Page Locators for chess.com
     """
     HIGHLIGHT = (By.CLASS_NAME, "highlight")
     GAME_OVER_MODAL = (By.ID, "game-over-modal")
-    BOARD = (By.ID, "board-vs-personalities")
+    # BOARD = (By.ID, "board-vs-personalities")
+    BOARD = (By.ID, "board-play-computer")
     INITIAL_MODAL = (By.CLASS_NAME, "modal-first-time-button")
     PROMOTION_WINDOW = (By.CLASS_NAME, "promotion-window")
